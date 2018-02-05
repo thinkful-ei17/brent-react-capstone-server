5665810bda3d54f5c415445435707f2033abd0b3
Brent Guistwite <brentguistwite@gmail.com>
2018-02-04 19:47:32 -0500

    change env vars

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://localhost/react-capstone',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test',
+        'mongodb://localhost/react-capstone-test',
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
   // TEST_DATABASE_URL:
